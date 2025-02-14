3b4e6aae98484ae2d0a2b9e7b8912a702a67689e
Jorgerohi2314 <jorgerodriguezhidalgo@gmail.com>
2025-02-14 13:41:19 +0100

    Creacion de vehiculo

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
@@ -1,6 +1,28 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
 public record Vehiculo() {
-    private static final String ER_MARCA ;
+    private static final String ER_MARCA = "[A-ZÁÉÍÓÚÜ]*[a-záéíóúü]*[ |-]?[A-ZAÉÍÓÚÜ]*[a-záéíóúü]*" ;
     private static final String ER_MATRICULA = "[0-9]{4}[B-Z(^EIOU)]{3}";
+
+    private String marca;
+    private String modelo;
+    private String matricula;
+
+    public Vehiculo(String marca, String modelo, String matricula) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
+
+    private void validadMarca(String marca) {
+
+    }
+
+    private void validarModelo(String modelo) {
+
+    }
+
+    private void validarMatricula(String matricula) {
+
+    }
 }
