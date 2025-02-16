21e10f30690c1387da9435c203d60ce757220971
Jorgerohi2314 <jorgerodriguezhidalgo@gmail.com>
2025-02-16 03:04:55 +0100

    Clase vehiculo, completada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
@@ -1,28 +1,61 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
-public record Vehiculo() {
-    private static final String ER_MARCA = "[A-ZÁÉÍÓÚÜ]*[a-záéíóúü]*[ |-]?[A-ZAÉÍÓÚÜ]*[a-záéíóúü]*" ;
+import java.util.Objects;
+
+public record Vehiculo(String marca, String modelo, String matricula) {
+    private static final String ER_MARCA = "[A-ZÁÉÍÓÚÜ]+[a-záéíóúü]*[ |-]?[A-ZÁÉÍÓÚÜ]{0,1}[a-záéíóúü]*" ;
     private static final String ER_MATRICULA = "[0-9]{4}[B-Z(^EIOU)]{3}";
 
-    private String marca;
-    private String modelo;
-    private String matricula;
 
     public Vehiculo(String marca, String modelo, String matricula) {
+        validadMarca(marca);
+        validarMatricula(matricula);
+        validarModelo(modelo);
         this.marca = marca;
         this.modelo = modelo;
         this.matricula = matricula;
     }
 
     private void validadMarca(String marca) {
-
+        Objects.requireNonNull(marca, "La marca no puede ser nula.");
+        if (!marca.matches(ER_MARCA)) {
+            throw new IllegalArgumentException("La marca no tiene un formato válido.");
+        }
     }
 
     private void validarModelo(String modelo) {
-
+        Objects.requireNonNull(modelo, "El modelo no puede ser nulo.");
+        if (modelo.trim().isEmpty()) {
+            throw new IllegalArgumentException("El modelo no puede estar en blanco.");
+        }
     }
 
     private void validarMatricula(String matricula) {
+        Objects.requireNonNull(matricula, "La matrícula no puede ser nula.");
+        if (!matricula.matches(ER_MATRICULA)) {
+            throw new IllegalArgumentException("La matrícula no tiene un formato válido.");
+        }
+    }
 
+    public static Vehiculo get(String matricula) {
+        Objects.requireNonNull(matricula, "La matrícula no puede ser nula.");
+        return new Vehiculo("Audi", "A4", matricula);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vehiculo vehiculo)) return false;
+        return Objects.equals(matricula, vehiculo.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matricula);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s - %s", marca, modelo, matricula);
     }
 }
