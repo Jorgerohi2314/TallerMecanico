7345fb64db38db051e9c8448a84ee2767f5b1250
Jorgerohi2314 <jorgerodriguezhidalgo@gmail.com>
2025-02-16 03:06:19 +0100

    Clase vehiculo, completada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public record Vehiculo(String marca, String modelo, String matricula) {
     private static final String ER_MARCA = "[A-ZÁÉÍÓÚÜ]+[a-záéíóúü]*[ |-]?[A-ZÁÉÍÓÚÜ]?[a-záéíóúü]*" ;
-    private static final String ER_MATRICULA = "[0-9]{4}[B-Z(^EIOU)]{3}";
+    private static final String ER_MATRICULA = "\\d{4}[B-Z(^EIOU)]{3}";
 
 
     public Vehiculo(String marca, String modelo, String matricula) {
