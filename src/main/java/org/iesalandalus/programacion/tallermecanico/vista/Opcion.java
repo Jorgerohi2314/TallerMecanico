df27cf7a64a12788658e94ee227346e8ccecf7ef
Jorgerohi2314 <jorgerodriguezhidalgo@gmail.com>
2025-03-12 10:53:58 +0100

    Opcion corregida y terminada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -4,30 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Opcion {
-    INSERTAR_CLIENTE(1, "Insertar un cliente"),
-    BUSCAR_CLIENTE(2, "Buscar un cliente"),
-    BORRAR_CLIENTE(3, "Borrar un cliente"),
-    LISTAR_CLIENTES(4, "Listar todos los clientes"),
-    MODIFICAR_CLIENTES(5, "Modificar un cliente"),
-    INSERTAR_VEHICULO(6, "Insertar un vehículo"),
-    BUSCAR_VEHICULO(7, "Buscar un vehículo"),
-    BORRAR_VEHICULO(8, "Borrar un vehículo"),
-    LISTAR_VEHICULOS(9, "Listar todos los vehículos"),
-    INSERTAR_REVISION(10, "Insertar una revisión"),
-    BUSCAR_REVISION(11, "Buscar una revisión"),
-    BORRAR_REVISION(12, "Borrar una revisión"),
-    LISTAR_REVISIONES(13, "Listar todas las revisiones"),
-    LISTAR_REVISIONES_CLIENTE(14, "Listar todas las revisiones de un cliente"),
-    LISTAR_REVISIONES_VEHICULO(15, "Listar todas las revisiones de un vehículo"),
-    ANADIR_HORAS_REVISION(16, "Añadir horas a una revisión"),
-    ANADIR_PRECIO_MATERIAL_REVISION(17, "Añadir precio de material a una revisión"),
-    CERRAR_REVISION(18, "Cerrar una revisión"),
-    SALIR(19, "Salir");
+    INSERTAR_CLIENTE(11, "Insertar un cliente"),
+    BUSCAR_CLIENTE(12, "Buscar un cliente"),
+    BORRAR_CLIENTE(13, "Borrar un cliente"),
+    LISTAR_CLIENTES(14, "Listar todos los clientes"),
+    MODIFICAR_CLIENTES(21, "Modificar un cliente"),
+    INSERTAR_VEHICULO(21, "Insertar un vehículo"),
+    BUSCAR_VEHICULO(23, "Buscar un vehículo"),
+    BORRAR_VEHICULO(24, "Borrar un vehículo"),
+    LISTAR_VEHICULOS(25, "Listar todos los vehículos"),
+    INSERTAR_REVISION(31, "Insertar una revisión"),
+    BUSCAR_REVISION(32, "Buscar una revisión"),
+    BORRAR_REVISION(33, "Borrar una revisión"),
+    LISTAR_REVISIONES(34, "Listar todas las revisiones"),
+    LISTAR_REVISIONES_CLIENTE(35, "Listar todas las revisiones de un cliente"),
+    LISTAR_REVISIONES_VEHICULO(36, "Listar todas las revisiones de un vehículo"),
+    ANADIR_HORAS_REVISION(37, "Añadir horas a una revisión"),
+    ANADIR_PRECIO_MATERIAL_REVISION(38, "Añadir precio de material a una revisión"),
+    CERRAR_REVISION(39, "Cerrar una revisión"),
+    SALIR(0, "Salir");
 
     private final int numeroOpcion;
     private final String mensaje;
-
-    private static final Map<Integer,Opcion> opciones = new HashMap<Integer, Opcion>();
+    private static final Map<Integer,Opcion> opciones = new HashMap<>();
 
     static {
         for (Opcion opcion : Opcion.values()) {
@@ -35,21 +34,24 @@ public enum Opcion {
         }
     }
 
-    Opcion(int numeroOpcion, String mensaje) {
+    private Opcion(int numeroOpcion, String mensaje) {
         this.numeroOpcion = numeroOpcion;
         this.mensaje = mensaje;
     }
 
     public static boolean esValida(int numeroOpcion) {
-        return (numeroOpcion > 0 && numeroOpcion <= 19);
+        return (opciones.containsKey(numeroOpcion));
     }
 
     public static Opcion get(int numeroOpcion) {
+        if (!esValida(numeroOpcion)) {
+            throw new IllegalArgumentException("El numero de opcion no es válido");
+        }
         return opciones.get(numeroOpcion);
     }
 
     @Override
     public String toString() {
-        return String.format("[numeroOpcion=%s, mensaje=%s]", numeroOpcion, mensaje);
+        return String.format("%s - %s%n", numeroOpcion, mensaje);
     }
 }
